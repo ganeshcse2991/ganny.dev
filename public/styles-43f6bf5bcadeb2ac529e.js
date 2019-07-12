37015301aafe6f9299ab26e4a4a79b89e16e734e
Ganesh Ravi Shankar <ganesh@upshotly.com>
2019-07-12 11:31:36 +0530

    Removing public and changin gitignore

--- public/styles-43f6bf5bcadeb2ac529e.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{218:function(n,o,w){},219:function(n,o,w){}}]);
-//# sourceMappingURL=styles-43f6bf5bcadeb2ac529e.js.map
--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -84,9 +84,9 @@ const SEO = () => {
     <Helmet>
       <html lang={config.siteLanguage} />
       <title>{title}</title>
-      <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
+      {/* <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" /> */}
       <meta name="ganesh-ravi-shankar" content="Ganesh Ravi Shankar" />
       <link rel="shortcut icon" href="favicon.ico" />
       <meta name="msapplication-TileColor" content={config.backgroundColor} />
